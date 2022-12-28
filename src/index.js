f790501953fcbb56b6cc60b09e3e4b495355afd8
mgibson <matthew.jgibson43@gmail.com>
2022-12-28 15:06:02 -0500

    Update: Added empty functions and imported date-fns functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,76 @@
 import './style.css';
+import { format, getDay, isDate, isPast, compareAsc, isValid, isToday, isYesterday } from 'date-fns';
 
 const content = document.getElementById('content');
+const todoList = [];
+
+function inbox() {
+  content.innerHTML = '';
+
+  const inboxTitleBox = document.createElement('div');
+  const inboxTitle = document.createElement('h2');
+  const date = document.createElement('p');
+
+  const overDueBox = document.createElement('div');
+  const todayBox = document.createElement('div');
+  
+  const todoCard = document.createElement('div');
+  const todoTitle = document.createElement('h3');
+  const dueDate = document.createElement('p');
+
+  inboxTitle.textContent = 'Inbox';
+  date.textContent = today;
+
+  inboxTitleBox.appendChild(inboxTitle);
+  inboxTitleBox.appendChild(date);
+
+  content.appendChild(inboxTitleBox);
+}
+
+inbox();
+
+todoList.push(createTodo('Hello', 'Hello World', 'today', '1'));
+
+function createTodo(title, desc, dueDate, priority) {
+  return {
+    title,
+    desc,
+    dueDate,
+    priority
+  };
+}
+
+function addTodo() {
+  const title = document.querySelector('.title');
+  const desc = document.querySelector('.desc');
+  const dueDate = document.querySelector('.dueDate');
+  const priority = document.querySelector('.priority');
+
+  todos.push(createTodo(title, desc, dueDate, priority));
+}
+
+function sort() {
+
+}
+
+function updateToday() {
+  
+}
+
+function updateInbox() {
+
+}
+
+function updatePriorityOne() {
+
+}
+
+function updatePriorityTwo() {
+
+}
+
+function updatePriorityThree() {
+
+}
+
 
